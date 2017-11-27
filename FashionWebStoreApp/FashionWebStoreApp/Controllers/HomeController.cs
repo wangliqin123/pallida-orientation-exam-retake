c9f5bbc341c856dc8e5577867bb57e183da7666d
Lorand <szilagyi.lori@gmail.com>
2017-11-27 13:49:22 +0100

    feat(FashionWebStoreApp): endpoint with warehouse route works

--- FashionWebStoreApp/FashionWebStoreApp/Controllers/HomeController.cs
+++ FashionWebStoreApp/FashionWebStoreApp/Controllers/HomeController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using FashionWebStoreApp.Services;
 
 namespace FashionWebStoreApp.Controllers
 {
     public class HomeController : Controller
     {
+        FashionStoreService FashionStoreService;
+
+        public HomeController(FashionStoreService fashionStoreService)
+        {
+            FashionStoreService = fashionStoreService;
+        }
+
         [HttpGet]
         [Route("/warehouse")]
         public IActionResult Index()
@@ -15,19 +23,19 @@ namespace FashionWebStoreApp.Controllers
             return View(FashionStoreService.GetWarehouseItemList());
         }
 
-        [HttpPost]
-        [Route("/warehouse/summary")]
-        public IActionResult Summary()
-        {
-            return View();
-        }
+        //[HttpPost]
+        //[Route("/warehouse/summary")]
+        //public IActionResult Summary()
+        //{
+        //    return View();
+        //}
 
-        [HttpGet]
-        [Route("/warehouse/query")]
-        public IActionResult ApiQuery()
-        {
-            return Json(new { result = "ok", data = });
-        }
+        //[HttpGet]
+        //[Route("/warehouse/query")]
+        //public IActionResult ApiQuery()
+        //{
+        //    return Json(new { result = "ok", data = });
+        //}
         
 
     }
