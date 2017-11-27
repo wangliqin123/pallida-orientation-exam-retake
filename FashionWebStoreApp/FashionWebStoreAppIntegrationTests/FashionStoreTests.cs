656e5bde55871f2950ae9b21a1b006b5dbe2f0ec
Lorand <szilagyi.lori@gmail.com>
2017-11-27 14:31:53 +0100

    feat(FashionWebStoreApp): add test project with one endpoint test

---
+++ FashionWebStoreApp/FashionWebStoreAppIntegrationTests/FashionStoreTests.cs
@@ -0,0 +1,31 @@
+﻿using FashionWebStoreApp;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FashionWebStoreAppIntegrationTests
+{
+    public class FashionStoreTests
+    {
+        private readonly HttpClient Client;
+        private readonly TestServer Server;
+
+        public FashionStoreTests()
+        {
+            Server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = Server.CreateClient();
+        }
+
+        [Fact]
+        public async Task IndexShouldReturnOkStatus()
+        {
+            var response = await Client.GetAsync("/warehouse");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
