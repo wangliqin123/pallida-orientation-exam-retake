dd4ff7c704ed3836681c18bd7da0ee8800b242e4
Lorand <szilagyi.lori@gmail.com>
2017-11-27 13:18:30 +0100

    feat(FashionWebStoreApp): add entity layer

---
+++ FashionWebStoreApp/FashionWebStoreApp/Entities/FashionStoreContext.cs
@@ -0,0 +1,14 @@
+﻿using FashionWebStoreApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FashionWebStoreApp.Entities
+{
+    public class FashionStoreContext : DbContext
+    {
+        public FashionStoreContext(DbContextOptions<FashionStoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Warehouse> Warehouses { get; set; }
+    }
+}
